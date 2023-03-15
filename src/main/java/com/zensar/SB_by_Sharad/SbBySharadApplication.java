658c761c3c7a14386a934772daa84ab9ac3fb03b
omkar <omsatav77@gmail.com>
2023-03-15 16:04:22 +0530

    updated code

--- src/main/java/com/zensar/SB_by_Sharad/SbBySharadApplication.java
+++ src/main/java/com/zensar/SB_by_Sharad/SbBySharadApplication.java
@@ -2,13 +2,20 @@ package com.zensar.SB_by_Sharad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SbBySharadApplication {
+public class SbBySharadApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbBySharadApplication.class, args);
 	}
 
-	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
+
 }
